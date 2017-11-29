54f8ff75920be2d6a62720466e84d72dbcb6ec81
ColumbusTech <ColumbusTech@yandex.ru>
2017-11-29 16:40:42 +0300

    CMF Simpliest Exporter

---
+++ cmf.py
@@ -0,0 +1,100 @@
+bl_info = {
+    "name": "Export Columbus Model Format (CMF) file",
+    "category": "Import-Export",
+}
+
+import bpy
+
+def triangulate(face):
+    triangles = []
+    if len(face) == 4:
+        triangles.append([face[0], face[1], face[2]])
+        triangles.append([face[2], face[3], face[0]])
+    else:
+        triangles.append(face)
+
+    return triangles
+
+def faceValues(face, mesh, matrix):
+    fv = []
+    for verti in face.vertices:
+        fv.append((matrix * mesh.vertices[verti].co)[:])
+    return fv
+
+def faceToLine(face):
+    return " ".join([("%.6f %.6f %.6f" % v) for v in face] + ["\n"])
+
+def faces():
+    scene = bpy.context.scene
+
+    faces = []
+    for obj in bpy.context.selected_objects:
+        if obj.type != 'MESH':
+            try:
+                me = obj.to_mesh(scene, True, "PREVIEW")
+            except:
+                me = None
+            is_tmp_mesh = True
+        else:
+            me = obj.data
+            if not me.tessfaces and me.polygons:
+                me.calc_tessface()
+            is_tmp_mesh = False
+
+        if me is not None:
+            matrix = obj.matrix_world.copy()
+            for face in me.tessfaces:
+                fv = faceValues(face, me, matrix)
+                faces.extend(triangulate(fv))
+
+            if is_tmp_mesh:
+                bpy.data.meshes.remove(me)
+
+    return faces
+
+def writeFile(filepath=""):
+    triangles = faces()
+
+    file = open(filepath, "w")
+    file.write('COLUMBUS MODEL FILE')
+    for face in triangles:
+        file.write(faceToLine(face))
+    file.close()
+
+class ExportCMF(bpy.types.Operator):
+    bl_idname = "export_mesh.cmf"
+    bl_label = "Export CMF"
+    filename_ext = ".cmf"
+    filter_glob = bpy.props.StringProperty(default="*.cmf", options={'HIDDEN'})
+    filepath = bpy.props.StringProperty(subtype='FILE_PATH')
+
+    def execute(self, context):
+        #file = open(self.filepath, "w")
+        #file.write('COLUMBUS MODEL FILE')
+        #file.close()
+        writeFile(self.filepath)
+
+        return {'FINISHED'}
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        wm.fileselect_add(self)
+        return {'RUNNING_MODAL'}
+
+
+def menu_export(self, context):
+    self.layout.operator(ExportCMF.bl_idname, text="Columbus Model Format (.cmf)")
+
+def register():
+    bpy.utils.register_class(ExportCMF)
+
+
+def unregister():
+    bpy.utils.unregister_class(ExportCMF)
+
+if __name__ == "__main__":
+    register()
+
+
+
+
