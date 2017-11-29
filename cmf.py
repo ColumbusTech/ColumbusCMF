d0ccf762033a8fe3b979a5c45d472fff273820c1
ColumbusTech <ColumbusTech@yandex.ru>
2017-11-29 18:31:23 +0300

    Exporter module, some upgrates

--- cmf.py
+++ cmf.py
@@ -4,67 +4,6 @@ bl_info = {
 }
 
 import bpy
-import struct
-
-def triangulate(face):
-    triangles = []
-    if len(face) == 4:
-        triangles.append([face[0], face[1], face[2]])
-        triangles.append([face[2], face[3], face[0]])
-    else:
-        triangles.append(face)
-
-    return triangles
-
-def faceValues(face, mesh, matrix):
-    fv = []
-    for verti in face.vertices:
-        fv.append((matrix * mesh.vertices[verti].co)[:])
-    return fv
-
-def faceToLine(face):
-    return " ".join([("%.6f %.6f %.6f" % v) for v in face] + ["\n"])
-
-def faces():
-    scene = bpy.context.scene
-
-    faces = []
-    for obj in bpy.context.selectable_objects:
-        if obj.type != 'MESH':
-            try:
-                me = obj.to_mesh(scene, True, "PREVIEW")
-            except:
-                me = None
-            is_tmp_mesh = True
-        else:
-            me = obj.data
-            if not me.tessfaces and me.polygons:
-                me.calc_tessface()
-            is_tmp_mesh = False
-
-        if me is not None:
-            matrix = obj.matrix_world.copy()
-            for face in me.tessfaces:
-                fv = faceValues(face, me, matrix)
-                faces.extend(triangulate(fv))
-
-            if is_tmp_mesh:
-                bpy.data.meshes.remove(me)
-
-    return faces
-
-def writeFile(filepath=""):
-    triangles = faces()
-
-    file = open(filepath, "wb")
-    file.write(b'COLUMBUS MODEL FILE')
-    for face in triangles:
-        #file.write(struct.pack('>f>f>f', faceToLine(face)))
-        #tri = faceToLine(face)
-        for tri in face:
-            for val in tri:
-                file.write(struct.pack('>f', val))
-    file.close()
 
 class ExportCMF(bpy.types.Operator):
     bl_idname = "export_mesh.cmf"
@@ -73,8 +12,18 @@ class ExportCMF(bpy.types.Operator):
     filter_glob = bpy.props.StringProperty(default="*.cmf", options={'HIDDEN'})
     filepath = bpy.props.StringProperty(subtype='FILE_PATH')
 
+    select_only = bpy.props.BoolProperty(
+            name="Selection Only",
+            description="Export selected objects only",
+            default=False)
+
     def execute(self, context):
-        writeFile(self.filepath)
+        import cmf_export
+
+        cmf_export.writeFile(
+                self.filepath,
+                self.select_only)
+        
         print ('Successfully exported CMF file')
         return {'FINISHED'}
 
---
+++ cmf_export.py
@@ -0,0 +1,65 @@
+import bpy
+import struct
+
+def triangulate(face):
+    triangles = []
+    if len(face) == 4:
+        triangles.append([face[0], face[1], face[2]])
+        triangles.append([face[2], face[3], face[0]])
+    else:
+        triangles.append(face)
+
+    return triangles
+
+def faceValues(face, mesh, matrix):
+    fv = []
+    for verti in face.vertices:
+        fv.append((matrix * mesh.vertices[verti].co)[:])
+    return fv
+
+def faceToLine(face):
+    return " ".join([("%.6f %.6f %.6f" % v) for v in face] + ["\n"])
+
+def faces(select_only=False):
+    scene = bpy.context.scene
+    selection = bpy.context.selectable_objects
+    if select_only:
+    	selection = bpy.context.selected_objects
+
+    faces = []
+    for obj in selection:
+        if obj.type != 'MESH':
+            try:
+                me = obj.to_mesh(scene, True, "PREVIEW")
+            except:
+                me = None
+            is_tmp_mesh = True
+        else:
+            me = obj.data
+            if not me.tessfaces and me.polygons:
+                me.calc_tessface()
+            is_tmp_mesh = False
+
+        if me is not None:
+            matrix = obj.matrix_world.copy()
+            for face in me.tessfaces:
+                fv = faceValues(face, me, matrix)
+                faces.extend(triangulate(fv))
+
+            if is_tmp_mesh:
+                bpy.data.meshes.remove(me)
+
+    return faces
+
+def writeFile(filepath="", select_only=False):
+    triangles = faces(select_only)
+
+    file = open(filepath, "wb")
+    file.write(b'COLUMBUS MODEL FILE') #Write magic
+    file.write(struct.pack('>i', len(triangles * 3))) #Write vertices count
+
+    for face in triangles:
+        for tri in face:
+            for val in tri:
+                file.write(struct.pack('>f', val)) #Write vertices
+    file.close()
