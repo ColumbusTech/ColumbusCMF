b9b01657a73b9096af13927cb8d3c9be29f9db46
ColumbusTech <ColumbusTech@yandex.ru>
2018-10-26 16:14:13 +0300

    Python code counting script

--- codelines.py
+++ codelines.py
@@ -12,7 +12,7 @@ for root, dirs, files in os.walk("."):
 			num_cpp_lines += sum(1 for line in open(os.path.join(root, filename)))
 		if filename.lower().endswith(('.h', '.hpp', '.hxx', '.h++', '.hh')):
 			num_cpp_header_lines += sum(1 for line in open(os.path.join(root, filename)))
-		if filename.lower().endswith(('.py', '.pyw', 'pyc', '.pyo', '.pyd')):
+		if filename.lower().endswith(('.py', '.pyw', '.pyc', '.pyo', '.pyd')):
 			num_python_lines += sum(1 for line in open(os.path.join(root, filename)))
 
 
