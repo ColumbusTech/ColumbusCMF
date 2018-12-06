6aacdfb9923c5c0f74a363c24be11b34163d0b94
ColumbusTech <ColumbusTech@yandex.ru>
2018-12-06 20:37:58 +0300

    Addon

--- addon/cmf.py
+++ addon/cmf.py
@@ -22,13 +22,49 @@ class ExportCMF(bpy.types.Operator):
             description="Smooth object normals",
             default=False)
 
+    write_indexes = bpy.props.BoolProperty(
+    		name="Write indexes",
+    		description="Create and write indexes",
+    		default=False)
+
+    write_positions = bpy.props.BoolProperty(
+    		name="Write positions",
+    		description="Write vertex positions",
+    		default=True)
+
+    write_texcoords = bpy.props.BoolProperty(
+    		name="Write texcoords",
+    		description="Write texture coordinates",
+    		default=True)
+
+    write_normals = bpy.props.BoolProperty(
+    		name="Write normals",
+    		description="Write normals",
+    		default=True)
+
+    write_tangents = bpy.props.BoolProperty(
+    		name="Write tangents",
+    		description="Write tangents",
+    		default=False)
+
+    write_colors = bpy.props.BoolProperty(
+    		name = "Write colors",
+    		description="Write vertex colors",
+    		default=False)
+
     def execute(self, context):
         import cmf_export
 
         cmf_export.writeFile(
                 self.filepath,
                 self.select_only,
-                self.smooth_normals)
+                self.smooth_normals,
+                self.write_indexes,
+                self.write_positions,
+                self.write_texcoords,
+                self.write_normals,
+                self.write_tangents,
+                self.write_colors)
         
         print ('Successfully exported CMF file')
         return {'FINISHED'}
@@ -44,7 +80,6 @@ def menu_export(self, context):
 
 def register():
     bpy.utils.register_class(ExportCMF)
-
 
 def unregister():
     bpy.utils.unregister_class(ExportCMF)
--- addon/cmf_export.py
+++ addon/cmf_export.py
@@ -1,12 +1,91 @@
 import bpy
 import bmesh
 import struct
+from enum import IntEnum
 
-def writeFile(filepath="", select_only=False, smooth_normals=False):
-    file = open(filepath, "wb")
-    file.write(b"COLUMBUS MODEL FORMAT")
+class Compression(IntEnum):
+    No   = 1 << 0
+    ZSTD = 1 << 1
 
-    count = 0
+class Type(IntEnum):
+    Positions = 0
+    Texcoords = 1
+    Normals   = 2
+    Tangents  = 3
+    Colors    = 4
+
+class Format(IntEnum):
+    Byte   = 0
+    UByte  = 1
+    Short  = 2
+    UShort = 3
+    Int    = 4
+    UInt   = 5
+    Half   = 6
+    Float  = 7
+    Double = 8
+
+def writeArray(file, array_type, array_format, array):
+    element_size = 0
+    element_pack = ""
+
+    if array_format is Format.Byte:
+        element_size = 1
+        element_pack = "b"
+    elif array_format is Format.UByte:
+        element_size = 1
+        element_pack = "B"
+    elif array_format is Format.Short:
+        element_size = 2
+        element_pack = "h"
+    elif array_format is Format.UShort:
+        element_size = 2
+        element_pack = "H"
+    elif array_format is Format.Int:
+        element_size = 4
+        element_pack = "i"
+    elif array_format is Format.UInt:
+        element_size = 4
+        element_pack = "I"
+    #elif array_format is Format.Half:
+    elif array_format is Format.Float:
+        element_size = 4
+        element_pack = "f"
+    elif array_format is Format.Double:
+        element_size = 8
+        element_pack = "d"
+
+    file.write(struct.pack("I", array_type))
+    file.write(struct.pack("I", array_format))
+    file.write(struct.pack("I", len(array) * element_size))
+
+    for elem in array:
+        file.write(struct.pack(element_pack, elem))
+
+def writeFile(filepath="",
+              select_only=False,
+              smooth_normals=False,
+              write_indexes=False,
+              write_positions=True,
+              write_texcoords=True,
+              write_normals=True,
+              write_tangents=False,
+              write_colors=False):
+
+    magic = b"COLUMBUS MODEL FORMAT  \0"
+    version = 1
+    filesize = 0
+    flags = 0
+    compression = Compression.No
+    num_vertices = 0
+    num_arrays = 0
+
+    verts = []
+    uvs = []
+    norms = []
+    tangs = []
+    cols = []
+
     for ob in bpy.context.scene.objects:
         try:
             ob.data.polygons
@@ -18,45 +97,37 @@ def writeFile(filepath="", select_only=False, smooth_normals=False):
         bpy.ops.mesh.quads_convert_to_tris()
         bpy.ops.object.mode_set(mode='OBJECT')
 
-        count+=len(ob.data.polygons)
-
-    file.write(struct.pack("I", count))
-    file.write(struct.pack("=B", 0x00))
-    bpy.ops.object.mode_set(mode='OBJECT')
-
-    verts = []
-    uvs = []
-    norms = []
-
-    for ob in bpy.context.scene.objects:
-        try:
-            ob.data.polygons
-        except AttributeError:
-            continue
-
         for face in ob.data.polygons:
-            for vert, loop in zip(face.vertices, face.loop_indices):
-                for item in ob.data.vertices[vert].co: #Vertex
-                    verts.append(item)
-                for item in (ob.data.uv_layers.active.data[loop].uv if ob.data.uv_layers.active != None else (0, 0)): #UV
-                    uvs.append(item)
-                #Smoothed/Not smoothed
-                if smooth_normals:
-                    for item in ob.data.vertices[vert].normal: #Vertex normal
-                        norms.append(item)
-                else: #Face normal
-                    vert = 0
-                    norms.append(face.normal.x)
-                    norms.append(face.normal.y)
-                    norms.append(face.normal.z)
+           for vert, loop in zip(face.vertices, face.loop_indices):
+                num_vertices += 1
 
-        for elem in verts:
-            file.write(struct.pack('f', elem))
+                verts.extend(ob.data.vertices[vert].co)
+                uvs.extend(ob.data.uv_layers.active.data[loop].uv if ob.data.uv_layers.active != None else (0, 0))
+                norms.extend(ob.data.vertices[vert].normal if smooth_normals else face.normal.xyz)
+                cols.extend(ob.data.vertex_colors.active.data[loop].color if ob.data.vertex_colors.active != None else (0, 0, 0))
 
-        for elem in uvs:
-            file.write(struct.pack('f', elem))
+    if write_positions: num_arrays += 1
+    if write_texcoords: num_arrays += 1
+    if write_normals:   num_arrays += 1
+    if write_tangents:  num_arrays += 1
+    if write_colors:    num_arrays += 1
 
-        for elem in norms:
-            file.write(struct.pack('f', elem))
+    file = open(filepath, "wb")
+
+    file.write(magic)
+    file.write(struct.pack("I", version))
+    file.write(struct.pack("I", filesize))
+    file.write(struct.pack("I", flags))
+    file.write(struct.pack("I", compression))
+    file.write(struct.pack("I", num_vertices))
+    file.write(struct.pack("I", num_arrays))
+
+    if write_positions: writeArray(file, Type.Positions, Format.Float, verts)
+    if write_texcoords: writeArray(file, Type.Texcoords, Format.Float, uvs)
+    if write_normals:   writeArray(file, Type.Normals,   Format.Float, norms)
+    if write_tangents:  writeArray(file, Type.Tangents,  Format.Float, tangs)
+    if write_colors:    writeArray(file, Type.Colors,    Format.Float, cols)
+
+    file.close()
 
 
